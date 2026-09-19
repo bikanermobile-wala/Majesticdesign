d2b46ab9a589f012e00f7b6bfd2caff294a0a76f
bikanermobile-wala <85690461+bikanermobile-wala@users.noreply.github.com>
2026-09-19 13:15:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './styles/global.css'
+import "./global.css";
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
